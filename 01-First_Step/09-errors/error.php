cd665d2b6dd8d464908c7a13e213d54261d0d91e
Farhan <ncem888@gmail.com>
2025-03-19 10:28:10 +0800

    How to deal with error and warning message

---
+++ 01-First_Step/09-errors/error.php
@@ -0,0 +1,15 @@
+<?php
+
+// Example of parse error (syntax) - No $ in variable
+// pageTitle = 'A Restaurant Website';
+
+// Example of undefined variable
+// $pageTitle = 'A Restaurant Website';
+// echo $pagetitle;
+
+// Example of errors - No such file or directory
+// include 'doesnotexist.php';
+
+// Fatal error
+// -It's an error that PHP can't recover from
+// -This means that the execution of the program has been terminated
